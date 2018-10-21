fadaded69db2cfa48dbad6003ccd4a8340e6a00a
MUKESHSIHAG <sihagmukesh22@gmail.com>
2018-10-21 22:51:45 +0530

    added cart model

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash')
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user')
@@ -28,7 +29,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'mysuppersecret', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'mysuppersecret',
+   resave: false, 
+   saveUninitialized: false,
+   store: new MongoStore({mongooseConnection: mongoose.connection}),
+   cookie: {maxAge: 180*60*1000}
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -36,6 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next){
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 

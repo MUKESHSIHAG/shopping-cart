5992cbd705d3a27dfad5438d1222f7c0b79038d9
MUKESHSIHAG <sihagmukesh22@gmail.com>
2018-11-11 22:06:03 +0530

    todo-app

---
+++ todo-app/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var todoController = require('./controller/todoController');
+
+var app = express();
+
+//set template engine
+app.set('view-engine','ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire controller
+todoController(app);
+
+//listen to  port
+app.listen(3000);
+console.log('you are listening to port 3000');
